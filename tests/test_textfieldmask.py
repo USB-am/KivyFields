3297537d37625bba3b7e624d9e11af630b4b87a3
USB-am <smit.acer@mail.ru>
2025-01-07 02:42:53 +0300

    Updated tests for mask_textfield

--- tests/test_textfieldmask.py
+++ tests/test_textfieldmask.py
@@ -2,7 +2,7 @@ import unittest
 
 from tests import AppTest
 
-from text_field_mask_test import MaskField
+from mask_textfield import MaskField
 
 
 class TestTextFieldMask(unittest.TestCase):
@@ -11,7 +11,13 @@ class TestTextFieldMask(unittest.TestCase):
 	@classmethod
 	def setUpClass(cls):
 		cls.app = AppTest()
-		cls.mask_field = MaskField(mask='+7 (___) ___-__-__')
+
+		cls.mask = '+7 (___) ___-__-__'
+		cls.mask_field = MaskField(
+			mask=cls.mask,
+			symbol='_',
+			only_digits=True
+		)
 		cls.app.screen.add_widget(cls.mask_field)
 
 	@classmethod
@@ -21,8 +27,26 @@ class TestTextFieldMask(unittest.TestCase):
 	def setUp(self):
 		self.mask_field.clear()
 
-	def test_oasdjaiosdj(self):
-		self.assertEqual(True, 1==1)
+	def test_update_text(self):
+		self.mask_field.memory = '8005553535'
+		self.mask_field.update_text()
+		self.assertEqual(self.mask_field.text, '+7 (800) 555-35-35')
+
+		self.mask_field.memory = '800555'
+		self.mask_field.update_text()
+		self.assertEqual(self.mask_field.text, '+7 (800) 555-__-__')
+
+	def test_insert_text(self):
+		self.mask_field.insert_text('q')
+		self.assertEqual(self.mask_field.text, '+7 (___) ___-__-__')
+
+		self.mask_field.insert_text('8')
+		self.assertEqual(self.mask_field.text, '+7 (8__) ___-__-__')
+
+	def test_do_backspace(self):
+		self.mask_field.memory = '8005553535'
+		self.mask_field.do_backspace()
+		self.assertEqual(self.mask_field.text, '+7 (800) 555-35-3_')
 
 
 if __name__ == '__main__':
